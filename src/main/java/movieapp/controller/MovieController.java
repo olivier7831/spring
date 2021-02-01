e213f9503ab1a227382c6b61d8b04dc2804f2759
Olivier Routier <olivier.routier@cimpa.com>
2021-02-01 17:11:20 +0100

    jour 2

--- src/main/java/movieapp/controller/MovieController.java
+++ src/main/java/movieapp/controller/MovieController.java
@@ -4,17 +4,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import movieapp.entity.Movie;
 import movieapp.persistance.MovieRepository;
 
+@Transactional
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
@@ -69,4 +75,39 @@ public class MovieController {
 		System.out.println(movies.toString());
 		return movies;
 	}
+	
+	@PutMapping
+	public Optional<Movie> updateMovie(@RequestBody Movie movie) {
+		Optional<Movie> optMovieDb = movieRepository.findById(movie.getId());
+		optMovieDb.ifPresent(m -> {
+			m.setDirector(movie.getDirector());
+			m.setDuration(movie.getDuration());
+			m.setId(movie.getId());
+			m.setTitle(movie.getTitle());
+			m.setYear(movie.getYear());
+		});
+		System.out.println(optMovieDb.toString());
+		return optMovieDb;
+	}
+	
+	@DeleteMapping
+	public Optional<Movie> deleteMovie(@RequestBody Movie movie) {
+		return deleteMovie(movie.getId());
+	}
+	
+	@DeleteMapping("/{id}")
+	public Optional<Movie> deleteMovie(@PathVariable("id") int id) {
+		Optional<Movie> optMovieDb = movieRepository.findById(id);
+		
+		optMovieDb.ifPresent(m -> movieRepository.deleteById(id));
+		System.out.println(optMovieDb.toString());
+		return optMovieDb;
+	}
+	
+	@GetMapping("/byTitle")
+	@ResponseBody
+	public List<Movie> moviesByTitle(@RequestParam String title) {
+		return movieRepository.findByTitle(title);
+	}
+	
 }
--- src/main/java/movieapp/persistance/MovieRepository.java
+++ src/main/java/movieapp/persistance/MovieRepository.java
@@ -1,9 +1,14 @@
 package movieapp.persistance;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import movieapp.entity.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+	
+	List<Movie> findByTitle(String title);
+	List<Movie> findByYear(int year);
 
 }
